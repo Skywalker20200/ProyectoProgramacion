ccdc0a9bcfbe4a87dd3c066cb97b74873ac3b393
Skywalker20200 <andreasct22@gmail.com>
2025-03-02 11:00:26 -0400

    Agregando clase Cadenas.cs

---
+++ Cadenas.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace practico
+{
+    class Cadenas
+    {
+        public void ejercicio1()
+        {
+            Console.Write("Ingrese una cadena: ");
+            string texto = Console.ReadLine();
+            Console.WriteLine("Longitud: " + texto.Length);
+        }
+    }
+}
